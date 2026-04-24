ecdbe0c3449805d3eb39959b4befd1fb8ceefd85
Sudan Basnet <sdnbasnet5@gmail.com>
2026-04-25 00:16:29 +1000

    uncontrolled component

--- src/MyForm.js
+++ src/MyForm.js
@@ -1,20 +1,33 @@
+import { useRef } from "react";
+
 export const Myform = () => {
+  const addressRef = useRef("");
+
   const handleOnSubmit = (e) => {
     e.preventDefault();
+    console.log(addressRef.current.value);
   };
   const handleOnChange = (e) => {
     const str = e.target.value;
     console.log(str);
   };
+
   return (
     <form action="" className="card" onSubmit={handleOnSubmit}>
       <div className="mb-3 mt-3 ">
+        {/* <input
+          type="text"
+          className="form-control mb-3"
+          required
+          placeholder="name"
+          onChange={handleOnChange}
+        /> */}
         <input
           type="text"
           className="form-control"
           required
-          placeholder="name"
-          onChange={handleOnChange}
+          placeholder="address"
+          ref={addressRef}
         />
       </div>
       <button className="btn btn-primary">Submit</button>
